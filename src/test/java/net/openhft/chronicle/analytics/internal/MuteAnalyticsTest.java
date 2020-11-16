76e15b704d78e5828a836975423f2b08ea6b192f
Per Minborg <minborg@speedment.com>
2020-11-16 16:54:48 +0100

    Add assertion i test

--- src/test/java/net/openhft/chronicle/analytics/internal/MuteAnalyticsTest.java
+++ src/test/java/net/openhft/chronicle/analytics/internal/MuteAnalyticsTest.java
@@ -1,13 +1,17 @@
 package net.openhft.chronicle.analytics.internal;
 
 import net.openhft.chronicle.analytics.Analytics;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 final class MuteAnalyticsTest {
 
     @Test
     void sendEvent() {
         final Analytics analytics = MuteAnalytics.INSTANCE;
+        assertDoesNotThrow(() -> analytics.sendEvent("a"));
         analytics.sendEvent("a"); // check it doesn't throw.
     }
 }
