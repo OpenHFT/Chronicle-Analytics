57301947f897dc4bbb7d23e8f32b516f57ecde27
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-11-16 16:01:43 +0000

    Code tidy and add to Build-All, closes https://github.com/OpenHFT/Chronicle-Analytics/issues/6

--- src/test/java/net/openhft/chronicle/analytics/internal/MuteAnalyticsTest.java
+++ src/test/java/net/openhft/chronicle/analytics/internal/MuteAnalyticsTest.java
@@ -4,6 +4,10 @@ import net.openhft.chronicle.analytics.Analytics;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 final class MuteAnalyticsTest {
@@ -11,7 +15,11 @@ final class MuteAnalyticsTest {
     @Test
     void sendEvent() {
         final Analytics analytics = MuteAnalytics.INSTANCE;
+        int events = MuteAnalytics.INSTANCE.mutedEvents;
         assertDoesNotThrow(() -> analytics.sendEvent("a"));
         analytics.sendEvent("a"); // check it doesn't throw.
+        analytics.sendEvent("a", Collections.emptyMap()); // check it doesn't throw.
+        // keep Sonar happy
+        assertEquals(2, MuteAnalytics.INSTANCE.mutedEvents - events);
     }
 }
