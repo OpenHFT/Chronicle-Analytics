a5dfeadb1c7bafd3049aa47b314fcc69e6a4c3f8
Per Minborg <minborg@speedment.com>
2020-11-18 15:14:16 +0100

    Remove contents and add info

--- src/main/java/net/openhft/chronicle/analytics/internal/InternalAnalyticsException.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.chronicle.analytics.internal;
-
-final class InternalAnalyticsException extends RuntimeException {
-
-    InternalAnalyticsException(String message) {
-        super(message);
-    }
-}
