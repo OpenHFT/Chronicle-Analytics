31218e2b53bd09a7f90d2236ec375fcb8c9305ac
Peter Lawrey <peter.lawrey@chronicle.software>
2024-04-12 09:32:52 +0100

    Added fail on warning lint for Javadoc, and rawtypes fixes for Bytes (#28)

--- src/main/java/net/openhft/chronicle/analytics/internal/InternalAnalyticsException.java
+++ src/main/java/net/openhft/chronicle/analytics/internal/InternalAnalyticsException.java
@@ -20,6 +20,7 @@ package net.openhft.chronicle.analytics.internal;
 
 @SuppressWarnings("serial")
 final class InternalAnalyticsException extends RuntimeException {
+    private static final long serialVersionUID = 0L;
 
     InternalAnalyticsException(String message) {
         super(message);
