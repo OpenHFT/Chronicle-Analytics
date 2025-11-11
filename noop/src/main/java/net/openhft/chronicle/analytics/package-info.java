a168bb236d63b30e1c73cabc3fa321abdb0da399
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 11:32:47 +0000

    Refactor copyright comments for consistency

--- noop/src/main/java/net/openhft/chronicle/analytics/package-info.java
+++ noop/src/main/java/net/openhft/chronicle/analytics/package-info.java
@@ -1,7 +1,6 @@
-//
-// Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
-//
-
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
 /**
  * This package contains the NOOP API for the chronicle-analytics library.
  */
