4b7f8227508206469aa3ba5629bb25e9df8027b4
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 16:00:53 +0000

    restore the javadoc

--- noop/src/main/java/net/openhft/chronicle/analytics/package-info.java
+++ noop/src/main/java/net/openhft/chronicle/analytics/package-info.java
@@ -1,4 +1,7 @@
 /*
  * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
+/**
+ * This package contains the NOOP API for the chronicle-analytics library.
+ */
 package net.openhft.chronicle.analytics;
