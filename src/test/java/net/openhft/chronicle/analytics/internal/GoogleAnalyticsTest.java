a3e37d33aa370067e8851f320ed4220fa8d4e835
Peter Lawrey <peter.lawrey@chronicle.software>
2026-03-23 13:24:26 +0000

    Remove dead disabled marker comments

--- src/test/java/net/openhft/chronicle/analytics/internal/GoogleAnalyticsTest.java
+++ src/test/java/net/openhft/chronicle/analytics/internal/GoogleAnalyticsTest.java
@@ -4,7 +4,6 @@
 package net.openhft.chronicle.analytics.internal;
 
 import org.jetbrains.annotations.NotNull;
-import org.junit.Assume;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
@@ -15,8 +14,8 @@ import java.util.function.Function;
 
 import static net.openhft.chronicle.analytics.internal.FilesUtil.removeLastUsedFileTimeStampSecond;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
-//@Disabled(/* failing test https://teamcity.chronicle.software/buildConfiguration/Chronicle_BuildAll_Build/677499?hideProblemsFromDependencies=false&hideTestsFromDependencies=false&expandBuildChangesSection=true&expandBuildTestsSection=true */)
 final class GoogleAnalyticsTest {
 
     @Test
@@ -92,7 +91,7 @@ final class GoogleAnalyticsTest {
         /* This test would fail if run concurrently from two different JVMs,
         therefore it's only run if the googleAnalytics instance is not muted (update in agreement with Rob)
         */
-        Assume.assumeFalse(googleAnalytics.muted);
+        assumeFalse(googleAnalytics.muted);
         for (int i = 0; i < messages; i++) {
             assertTrue(googleAnalytics.attemptToSend(), "Round " + i);
         }
